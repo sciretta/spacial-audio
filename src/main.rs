c6898536806e8d28d461a33e7d547b6bea8e5d56
sciretta <sciretta28@gmail.com>
2025-01-25 00:21:20 -0400

    add_buffer endpoint

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::convert::Infallible;
 use std::sync::{Arc, Mutex};
 use tokio::sync::Mutex as TokioMutex;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct AudioData {
     metadata: String,
     audio_buffer: Vec<Vec<u8>>,
@@ -45,19 +45,53 @@ async fn create_session(
         },
     );
 
-    // Print the current state of the HashMap
-    println!("Current sessions: {:?}", *sessions);
-
     Ok(Response::new(Body::from(
         "Audio buffer and metadata received",
     )))
 }
 
+async fn add_buffer(
+    req: Request<Body>,
+    state: Arc<AppState>,
+) -> Result<Response<Body>, Infallible> {
+    let metadata = req
+        .headers()
+        .get("metadata")
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let session_code = req
+        .headers()
+        .get("sessioncode")
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let whole_body = hyper::body::to_bytes(req.into_body()).await.unwrap();
+
+    // Store the received audio buffer and metadata in memory
+    let mut sessions = state.sessions.lock().await;
+    let session = sessions.get_mut(&session_code);
+
+    match session {
+        Some(session) => {
+            session.audio_buffer.push(whole_body.to_vec());
+            Ok(Response::new(Body::from(
+                "Audio buffer and metadata added successfully to the session",
+            )))
+        }
+        None => return Ok(Response::new(Body::from("Session not found"))),
+    }
+}
+
 async fn router(req: Request<Body>, state: Arc<AppState>) -> Result<Response<Body>, Infallible> {
     println!("Request received URI: {}", req.uri().path());
+    println!("Current sessions: {:?}", state.sessions);
 
     match req.uri().path() {
         "/create-session" => create_session(req, state).await,
+        "/add-buffer" => add_buffer(req, state).await,
         _ => Ok(Response::new(Body::from("Invalid path"))),
     }
 }
